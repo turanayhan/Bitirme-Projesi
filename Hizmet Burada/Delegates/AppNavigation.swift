7817f298e2b031eed40b93e1c9feb3a19edc084f
turan <duranayhan57@gmail.com>
2024-10-09 18:50:12 +0300

    sv

--- Hizmet Burada/Delegates/AppNavigation.swift
+++ Hizmet Burada/Delegates/AppNavigation.swift
@@ -30,7 +30,7 @@ public class AppNavigation {
     }
     func startApp() {
         
-        let root = DefaultNavigation(rootViewController:SplashScreen())
+        let root = DefaultNavigation(rootViewController:ReservationDetail())
         root.navigationBarBackgroundClear = true
         window!.rootViewController = root
         window!.makeKeyAndVisible()
